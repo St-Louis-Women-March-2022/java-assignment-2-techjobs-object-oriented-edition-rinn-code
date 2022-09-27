f9cb015b9f2c802c58fde8e7c3c381df8f1ad550
rinn-code <98275792+rinn-code@users.noreply.github.com>
2022-09-26 19:25:13 -0500

    part 3 passes

--- src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
+++ src/main/java/org/launchcode/techjobs/oo/test/JobTest.java
@@ -5,20 +5,22 @@ import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 import org.launchcode.techjobs.oo.Job;
 
+import static org.junit.Assert.assertNotEquals;
+
 /**
  * Created by LaunchCode
  */
 @RunWith(JUnit4.class)
 public class JobTest {
-/*
+/**/
     @Test
     public boolean testSettingJobID(){
         Job job1 = new Job();
         Job job2 = new Job();
-        Assert.assertNotEquals(job1.getId() == job2.getId());
+        assertNotEquals(job1.getId(), job2.getId());
 
-        return false;
-    }*/
+        return true;
+    }
 }
 
 
